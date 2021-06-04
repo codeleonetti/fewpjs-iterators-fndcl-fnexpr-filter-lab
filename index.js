3da6736834b626c850a26d0a7abb5f9afca84e63
john <john.d.leonetti@gmail.com>
2021-06-04 09:48:26 -0700

    commit tree b6df76e2225f536a2b1d70e18af86d6958b8e573

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const { stringContaining } = require("expect")
+
 // Code your solution here
+function findMatching(driversArray, name) {
+    return driversArray.filter(element =>  element.toUpperCase() === name.toUpperCase())
+}
+
+function fuzzyMatch(drivers, name){
+    //match object to string
+    //go through each driver
+    //select the name that matches the string
+    // return the string
+    
+    //return drivers.filter(element => element.substring(0, n) === name );
+    return drivers.filter(element => element.match("^"+name));
+}
+
+function matchName(drivers, name){
+    // go through drivers names
+    // match driver to name
+    // return matched name
+    return drivers.filter(element => element.name === name);
+}
+
+
+console.log("test to see")
+    console.log(name)
+    name.filter(names => console.log(`name list, ${names}`))
